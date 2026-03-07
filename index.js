ed74d66c88bc960452868f41e0d07d558d34224e
lonermoon50 <142154187+lonermoon50@users.noreply.github.com>
2026-03-07 12:16:29 +0530

    Enhance login validation with Firestore checks

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth, db } from "./firebase.js";
+
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { collection, query, where, getDocs } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const loginButton = document.getElementById("login");
 const message = document.getElementById("message");
 
+
 async function validateRoll() {
+
   const roll = usernameInput.value.trim().toUpperCase();
   const password = passwordInput.value.trim();
   const email = roll + "@coaet.edu";
-
-
 
   message.textContent = "";
 
@@ -23,21 +29,70 @@ async function validateRoll() {
   }
 
   try {
-    await signInWithEmailAndPassword(auth, email, password);
-    message.style.color = "green";
-    message.textContent = "Login successful";
-    message.style.display = "block";
 
-    setTimeout(() => {
-      window.location.href = "homepage.html";
-    }, 1000);
+   
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
 
-  } catch (err) {
+    console.log("Logged in UID:", user.uid);
+
+    
+    const q = query(
+      collection(db, "students"),
+      where("uid", "==", user.uid)
+    );
+
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+
+      const studentData = querySnapshot.docs[0].data();
+      console.log("Student Data:", studentData);
+
+      const studentClass = studentData.classYear;
+
+      message.style.color = "green";
+      message.textContent = "Login successful ";
+      message.style.display = "block";
+
+      setTimeout(() => {
+
+        if (studentClass === "1st Year") {
+          window.location.href = "1styear.html";
+        }
+
+        else if (studentClass === "2nd Year") {
+          window.location.href = "2ndyear.html";
+        }
+
+        else if (studentClass === "3rd Year") {
+          window.location.href = "3rdyear.html";
+        }
+
+        else {
+          window.location.href = "homepage.html";
+        }
+
+      }, 1000);
+
+    } 
+    else {
+      message.style.color = "red";
+      message.textContent = "Student record not found ";
+      message.style.display = "block";
+    }
+
+  } 
+  catch (err) {
+
+    console.error(err);
+
     message.style.color = "red";
     message.textContent = "Invalid Roll Number or Password ";
     message.style.display = "block";
+
   }
+
 }
 
 loginButton.addEventListener("click", validateRoll);
-
